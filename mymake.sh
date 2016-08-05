aa370b3db9609c9908e38b6318b4ed5d995f01b0
Takanori Watanabe (sbia-pc125) <Takanori.Watanabe@uphs.upenn.edu>
2016-08-05 14:53:53 -0400

    update: added penn favicon

--- mymake.sh
+++ mymake.sh
@@ -2,7 +2,8 @@
 #=============================================================================#
 # do all the make at once
 #=============================================================================#
-#make clean
+make clean
 make html
 bash fix_name.sh
+cp ./source/img/favicon-penn.ico ./build/html/static
 # rsync -r build/html/ published
